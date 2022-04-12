065fa4866c372f1f9d57dcf35073a4f1e302ab26
hitorigods <hitorigods1124@gmail.com>
2022-04-12 18:20:37 +0900

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-scp -r ./ hitorigods@s223.xrea.com:~/public_html/hitorigods.shop/lp-practice-static
+scp -r ./ hitorigods@s223.xrea.com:~/lp-practice-static
+# scp -r ./ hitorigods@s223.xrea.com:~/public_html/hitorigods.shop/lp-practice-static
