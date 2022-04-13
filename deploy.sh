46a8e044d7a02accb32ecfad1686a717cf8de74c
hitorigods <hitorigods1124@gmail.com>
2022-04-13 18:28:57 +0900

    rsyncのシェル追加

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-scp -r ./ hitorigods@s223.xrea.com:~/lp-practice-static
-# scp -r ./ hitorigods@s223.xrea.com:~/public_html/hitorigods.shop/lp-practice-static
+rsync -av ./ hitorigods@s223.xrea.com:~/lp-practice-static
